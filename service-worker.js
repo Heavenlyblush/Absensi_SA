a2ae73fd29b24a775d77b7b5ebfd961580b5275f
HeavenlyBlush <muh.wahyudin@heavenlyblush.com>
2024-10-18 13:08:45 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        'https://heavenlyblush.com/wp-content/uploads/2022/03/cropped-Logo_hb_2022-2-1.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
